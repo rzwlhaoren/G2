b68bae21f43c31d9c61f0e88db4cd07721c2316e
rzwlhaoren <56285094+rzwlhaoren@users.noreply.github.com>
2019-11-03 23:43:11 +0800

    Add files via upload

---
+++ text4/4.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+	int a,b,c;
+	scanf("%d%d%d",&a,&b,&c);
+	if(a==b&&a==c)printf("%d\n",a);
+	else if(a>b&&a>c)printf("%d\n",a);
+	else if(b>a&&b>c)printf("%d\n",b);
+	else if(a==b&&a>c)printf("%d\n",a);
+	else printf("%d\n",c);
+	return 0;
+}
